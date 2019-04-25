6ab3c0408daf2df496dc6ecbb048df8acc8c95bc
huangfuhua <huangfuhua@raycloud.com>
2019-04-25 14:25:43 +0800

    本地启动测试完成;

---
+++ springboot-web/src/main/java/com/hfh/springbootmultimodule/controller/TeskController.java
@@ -0,0 +1,12 @@
+package com.hfh.springbootmultimodule.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by huangfuhua on 2019/4/25.
+ */
+@RestController
+@RequestMapping(value = "test")
+public class TeskController {
+}
